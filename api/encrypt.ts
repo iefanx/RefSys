6fd64fc0e42802dc371c830e1333a75d582b4aff
iefan <66840141+iefanx@users.noreply.github.com>
2024-12-02 13:47:34 +0530

    Update encrypt.ts

--- api/encrypt.ts
+++ api/encrypt.ts
@@ -200,13 +200,7 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
       </div>
 
       <div class="flex justify-center space-x-4">
-        <a href="/api/encrypt" 
-          class="bg-gray-600 hover:bg-gray-700 rounded-lg px-6 py-3 text-center text-white font-bold inline-flex items-center space-x-2">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L4.414 9H17a1 1 0 110 2H4.414l5.293 5.293a1 1 0 010 1.414z" clip-rule="evenodd" />
-          </svg>
-          <span>Back to Home</span>
-        </a>
+        
         <a href="/api/encrypt?action=encrypt" 
           class="bg-green-600 hover:bg-green-700 rounded-lg px-6 py-3 text-center text-white font-bold inline-flex items-center space-x-2">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
