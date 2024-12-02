12afeafca0a571006c454d46b646de01df064bcf
iefan <66840141+iefanx@users.noreply.github.com>
2024-12-02 13:34:26 +0530

    Update encrypt.ts

--- api/encrypt.ts
+++ api/encrypt.ts
@@ -130,11 +130,11 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
 
       <div class="flex justify-center">
         <a href="/api/encrypt?action=decrypt&encryptedCN=${encodeURIComponent(encryptedData)}&iv=${iv}&id=${id}&version=${currentKeyVersion}" 
-          class="bg-green-600 hover:bg-green-700 rounded-lg px-6 py-3 text-center text-white font-bold inline-flex items-center space-x-2">
+          class="bg-green-600 hover:bg-green-700 rounded-lg px-4 text-sm py-2 text-center text-white font-bold inline-flex items-center space-x-2">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
           </svg>
-          <span>Generate Hash & Decrypt</span>
+          <span class="text-sm">Generate Hash & Decrypt</span>
         </a>
       </div>
     </div>`;
